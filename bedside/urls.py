2e7f2bf0ef670237b778df0da28e4a3907804ee3
Matteo Brucato <mattfeel@gmail.com>
2012-03-09 22:08:58 +0100

    little mods

--- bedside/urls.py
+++ bedside/urls.py
@@ -1,12 +1,12 @@
-from djangorestframework.resources import ModelResource
-from djangorestframework.views import ListOrCreateModelView, InstanceModelView
+#from djangorestframework.resources import ModelResource
+#from djangorestframework.views import ListOrCreateModelView, InstanceModelView
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.auth.views import login
 #from views import Givings, Giving
 
 # Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
+#from django.contrib import admin
+#admin.autodiscover()
 
 urlpatterns = patterns('bedside.views',
     # Examples:
@@ -16,10 +16,12 @@ urlpatterns = patterns('bedside.views',
     (r'^i18n/', include('django.conf.urls.i18n')),
 	#url(r'^mobi/', 'mobi'),
 	url(r'^app', 'app'),
-	url(r'^accounts/profile/$', 'givings'),
+	
+	
+	#url(r'^accounts/profile/$', 'givings'),
 	#url(r'^givings/$', Givings.as_view(), name='givings'),
-	url(r'^givings/$', 'givings'),
+	#url(r'^givings/$', 'givings'),
 	#url(r'^givings/(?P<gid>\d+)/$', Giving.as_view(), name='giving'),
-	url(r'^givings/compile/(?P<gid>\d+)/$', 'givings_compile_ui'),
-	url(r'^givings/getxml/(?P<gid>\d+)/$', 'givings_get_xml'),
+	#url(r'^givings/compile/(?P<gid>\d+)/$', 'givings_compile_ui'),
+	#url(r'^givings/getxml/(?P<gid>\d+)/$', 'givings_get_xml'),
 )
