2e80d0941a93b00839d8e1272ee273079d006795
Matteo Brucato <mattfeel@gmail.com>
2012-03-11 16:05:58 +0100

    tasti tabelle

--- bedside/static/js/bedside-clickable-tables.js
+++ bedside/static/js/bedside-clickable-tables.js
@@ -1,4 +1,4 @@
-$(function () {
+$('body').live("pagecreate", function(event) {
 	$(".clickabletable .clickableCell").click(function () {
 		/* get payoff from element */
 		var payoff = parseFloat($(this).attr("data-payoff"));
@@ -6,6 +6,19 @@ $(function () {
 		/* highlight only this element on this row */
 		$(this).siblings().removeClass("highlighted");
 		$(this).addClass("highlighted");
+		//alert($(this).find('button').attr('data-theme'));
+		//$(this).find('button').buttonMarkup({ theme: "a" });
+		
+		$(this).find('button').attr('data-theme', 'a');
+		$(this).find('button').button('refresh');
+		
+		//$(this).find("input[type='radio']").checkboxradio({ theme: "a" });
+		//$(this).find("input[type='radio']:first").attr("checked",true).checkboxradio("refresh");
+		
+		
+		//$(this).find('*[data-theme]').attr('data-theme', 'b');
+		//$(this).find('*[data-theme]').trigger('updatelayout');
+		//$(this).find('button').trigger('create');
 		/* update partial total */
 		$(this).siblings(".rowTotal").html(payoff);
 		
@@ -16,5 +29,12 @@ $(function () {
 			total += parseFloat($(this).text());
 		});
 		table.find(".total").html(total);
-	});		
+		
+		//$('#nurse1').trigger('pageshow');
+	});
+	
+	//$("button").live('click', function () {
+		//$(this).find('*[data-theme]').attr('data-theme', 'b');
+		//$(this).hide();
+	//});
 });
